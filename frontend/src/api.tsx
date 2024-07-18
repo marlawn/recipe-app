09a0f9859985e3e9af8b2130a0f470f1415c3917
Marlon Vergara <marlonvegara12@gmail.com>
2024-07-17 23:57:35 -0400

    Update project

--- frontend/src/api.tsx
+++ frontend/src/api.tsx
@@ -6,4 +6,14 @@ const api = axios.create({
     baseURL: API_URL,
 })
 
+export const getReviewForRecipe = async (recipeID : any) => {
+    try {
+        const response = await api.get("/reviews/?recipe_id=${recipe_id}");
+        return response.data;
+    } catch (e) {
+        console.error('Error fetching reviews:', e);
+        throw e;
+    }
+}
+
 export default api;
--- frontend/src/components/Recipes.tsx
+++ frontend/src/components/Recipes.tsx
@@ -1,13 +1,26 @@
+import useFetchRecipeID from "../hooks/useFetchRecipeID";
+import useFetchRecipes from "../hooks/useFetchRecipes";
 
 
 export const Recipes = () => {
+  const { recipes, loading, error } = useFetchRecipes();
+  const recipe = useFetchRecipeID(1).recipe;
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error.message}</div>;
+
+
   return (
     <>
-    {}
-    <div className="p-5 border-4">
-        <h1 className="text-2xl font-semibold">Title</h1>
-        <p>Description</p>
-    </div>
+    <div>{recipe}</div>
+    <h1>Recipes</h1>
+    <ul className="p-5 border-4">
+        {recipes.map(recipe => (
+          <li key={recipe['id']}>{recipe['title']}</li>
+        ))}
+        {/* <h1 className="text-2xl font-semibold">Title</h1>
+        <p>Description</p> */}
+    </ul>
     </>
   )
 }
